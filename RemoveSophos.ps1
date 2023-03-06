64ce835d9acf0a8b50bea965ec08f727a90f12e6
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2023-03-06 08:54:37 +0100

    Update RemoveSophos.ps1

--- RemoveSophos.ps1
+++ RemoveSophos.ps1
@@ -51,6 +51,25 @@ Get-Service -DisplayName Sophos* | ForEach-Object { & "sc.exe" "delete" $_.Name 
 
 # Remove Sophos registry entries (Critical)
 Write-Host "7. Remove all Sophos registry entries"
+Remove-Item -Path "HKLM:\SOFTWARE\Sophos" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SOFTWARE\Wow6432Node\Sophos" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Endpoint Defense" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SntpService" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Agent" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos AutoUpdate Service" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Client Firewall" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Client Firewall Manager" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos ELAM" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Endpoint Defense" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Endpoint Defense Service" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Message Router" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos System Protection Service" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sophos Web Control Service" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SophosBootDriver" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\swi_filter" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\swi_service" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SAVService" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SAVAdminService" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Sophos" -Recurse -Force
 Remove-Item -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Sophos" -Recurse -Force
 Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Sophos" -Recurse -Force
